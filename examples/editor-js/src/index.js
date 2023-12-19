bc5e8a5c96f167c39a41d36966d4278ca80752a6
Felix Marezki <fmarezki@gmail.com>
2023-12-19 13:46:43 +0100

    rename polymath-rs to polymath-web

--- examples/editor-js/src/index.js
+++ examples/editor-js/src/index.js
@@ -1,10 +1,10 @@
 import 'file-loader?name=[name].[ext]!../index.html';
 
-import("asciimath-js").then(asciimath => {
-    var input = document.getElementById("input");
-    var output = document.getElementById("output");
+import("polymath-web").then(asciimath => {
+  var input = document.getElementById("input");
+  var output = document.getElementById("output");
 
-    input.oninput = function () {
-        output.innerHTML = asciimath.asciimath_to_mathml(input.value);
-    }
+  input.oninput = function() {
+    output.innerHTML = asciimath.asciimath_to_mathml(input.value);
+  }
 });
