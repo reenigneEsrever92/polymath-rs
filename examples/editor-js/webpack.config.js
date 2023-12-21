b39f7a871e8521a6e788dd2b941e1abff117646f
Felix Marezki <fmarezki@gmail.com>
2023-12-21 14:51:48 +0100

    update polymath-web readme

--- examples/editor-js/webpack.config.js
+++ examples/editor-js/webpack.config.js
@@ -1,14 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    watch: true,
-    experiments: {
-        asyncWebAssembly: true,
-        syncWebAssembly: true
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js',
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
 };
